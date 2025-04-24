453181a00d23ccf60252213c0cd3fde7bdac7b7e
suhasjakati <suhasjakati219@gmail.com>
2025-04-24 11:34:27 +0530

    savefile

--- Demo.java
+++ Demo.java
@@ -3,7 +3,7 @@ public class Demo
     public static void main(String[] args) {
         int n1=10,n2=5;
         //Arithmetic operators 
-        System.out.println("The sum of n1 and n2 is: " + (n1+n2));
-        System.out.println("The difference of n1 and n2 is: " + (n1-n2));
+        System.out.println("The sum of n1 and n2 is: " + (n1*n2));
+        System.out.println("The difference of n1 and n2 is: " + (n1+n2));
     }
 }
